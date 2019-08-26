eebbb1b26b0af2c153dd708c610e9d742c67bf6d
Sergei Kalugin (Work iMac) <sergei.kalugin@northwestern.edu>
2019-08-26 13:28:07 -0500

    Finishes first version

--- OpenGraphTwitterMetatagsPlugin.php
+++ OpenGraphTwitterMetatagsPlugin.php
@@ -31,7 +31,7 @@ class OpenGraphTwitterMetatagsPlugin extends Omeka_Plugin_AbstractPlugin
     // Initialize variables to hold eventual values
     $title = '';
     $description = '';
-    $image_url = '';
+    $imageUrl = '';
 
     /**
      * Now, it depends on the page type what data to grab.
@@ -52,20 +52,13 @@ class OpenGraphTwitterMetatagsPlugin extends Omeka_Plugin_AbstractPlugin
      * - simple page
      */
 
-    //
+    // 1.
     $collection = get_current_record('collection', false);
     $item = get_current_record('item', false);
     $exhibit = get_current_record('exhibit', false);
     $file = get_current_record('file', false);
 
-    echo $collection ? 'collection-YES' : 'collection-NO';
-    echo $item ? 'item-YES' : 'item-NO';
-    echo $exhibit ? 'exhibit-YES' : 'exhibit-NO';
-    echo $file ? 'file-YES' : 'file-NO';
-
-    // 1.
     if ($collection || $item || $exhibit || $file) {
-      echo 'COL-ITE-EXH-FIL';
 
       // Collection overview page (collection/show/:id)
       if ($collection) {
@@ -73,49 +66,47 @@ class OpenGraphTwitterMetatagsPlugin extends Omeka_Plugin_AbstractPlugin
         $description = metadata('collection', array('Dublin Core', 'Description'));
 
         // This gets representative file for collection
-        $file = $collection->getFile();
+        $collectionFile = $collection->getFile();
 
-        if ($file) {
-          $image_url = file_display_url($file, 'fullsize');
+        if ($collectionFile && $collectionFile->hasFullsize()) {
+          $imageUrl = file_display_url($collectionFile);
         }
       }
 
-      // Single item
-      if ($item) {
+      // Single item and not a collection (because collections/show/:id
+      // has both 'collection' and 'item' set as current record; todo: why?)
+      if ($item && !$collection) {
         $title = metadata('item', array('Dublin Core', 'Title'));
         $description = metadata('item', array('Dublin Core', 'Description'));
 
-        if (strlen($title) > 0 && strlen($description) > 0) {
-          foreach (loop('files', $item->Files) as $file) {
-            if ($file->hasThumbnail()){
-              $image_url = file_display_url($file, 'thumbnail');
-              var_dump($image_url);
-              break;
-            }
-          }
+        $itemFile = $item->getFile();
+
+        if ($itemFile && $itemFile->hasFullsize()) {
+          $imageUrl = file_display_url($itemFile);
         }
       }
 
       // Exhibit page or exhibit landing page
       if ($exhibit) {
         $title = metadata($exhibit, 'title', array('no_escape' => false));
-        $description = metadata($exhibit, 'description', array('no_escape' => false));
+        $description = metadata($exhibit, 'description');
 
-        $file = $exhibit->getFile();
+        $exhibitFile = $exhibit->getFile();
 
-        if ($file) {
-          $image_url = file_display_url($file, 'fullsize');
-          print_r($image_url);
+        if ($exhibitFile && $exhibitFile->hasFullsize()) {
+          $imageUrl = file_display_url($exhibitFile);
         }
       }
 
       // File page
       if ($file) {
-        $title = metadata($file, 'original_filename');
-        $description = 'A file from the Open Door Archive.';
+        $title = metadata($file, 'display_title');
+        $description = 'A file from the Open Door Archive collection.';
 
-        if ($f = $file->getFile()) {
-          $image_url = file_display_url($f);
+        $fileFile = $file->getFile();
+
+        if ($fileFile && $fileFile->hasFullsize()) {
+          $imageUrl = file_display_url($fileFile);
         }
       }
 
@@ -125,61 +116,52 @@ class OpenGraphTwitterMetatagsPlugin extends Omeka_Plugin_AbstractPlugin
       has_loop_records('item') ||
       has_loop_records('exhibit')
     ) {
-      echo 'SOME KIND OF LIST';
+      // TODO: add case for the lists
+      // echo 'HAS LOOP RECORDS';
     } else {
-
+      // TODO: what else is possible?
       // 3.
-      echo 'Something else';
+      // echo 'NO RECORDS FOUND';
     }
 
-    var_dump(has_loop_records('collection'));
-    var_dump(has_loop_records('item'));
-    var_dump(has_loop_records('exhibit'));
-    var_dump(has_loop_records('file'));
-    var_dump(has_loop_records('page'));
-
-    var_dump(count(get_loop_records('collection', false)));
-    var_dump(count(get_loop_records('item', false)));
-    var_dump(count(get_loop_records('exhibit', false)));
-
-    // Default to the site settings if we didn't find anything else to use
-    if ($title === '' || $description === '') {
+    // If no title found above, then fall back to the defaults for the site...
+    if (!$title) {
       $title = option('site_title');
       $description = option('description');
-      $items = get_random_featured_items(1, true);
 
-      if (isset($items[0])){
-        foreach (loop('files', $items[0]->Files) as $file){
-          if($file->hasThumbnail()){
-            $image_url = file_display_url($file, 'thumbnail');
+      // ...and use one of the featured units image
+      $featuredItems = get_random_featured_items(5, true);
+
+      if (count($featuredItems) > 0) {
+        foreach ($featuredItems as $featuredItem) {
+          $featuredItemFile = $featuredItem->getFile();
+
+          if ($featuredItemFile && $featuredItemFile->hasFullsize()) {
+            $imageUrl = file_display_url($featuredItemFile);
             break;
           }
         }
       }
     }
 
-    // Now let's write the tags
-    if (strlen($title) > 0 && strlen($description) > 0) {
+    // Finally, writing meta tags
+    echo '<meta property="twitter:card" content="summary_large_image" />' . "\n";
+    echo '<meta property="twitter:site" content="' . get_option(OG_TWITTER_METATAGS_PLUGIN_OPTION) . '" />' . "\n";
 
-      // Twitter Card
-      echo '<meta property="twitter:card" content="summary" />';
-      echo '<meta property="twitter:site" content="'.get_option(OG_TWITTER_METATAGS_PLUGIN_OPTION).'" />';
-      echo '<meta property="twitter:title" content="'.strip_tags(html_entity_decode($title)).'" />'."\n";
-      echo '<meta property="twitter:description" content="'.strip_tags(html_entity_decode($description)).'" />'."\n";
+    if ($title) {
+      echo '<meta property="twitter:title" content="' . strip_tags($title) . '" />' . "\n";
+      echo '<meta property="og:title" content="' . htmlentities(strip_tags(html_entity_decode($title))) . '" />' . "\n";
 
-      if (strlen($image_url) > 0) {
-        echo '<meta property="twitter:image" content="'.$image_url.'" />'."\n";
+      if ($description) {
+        echo '<meta property="twitter:description" content="' . strip_tags($description) . '" />' . "\n";
+        echo '<meta property="og:description" content="' . htmlentities(strip_tags(html_entity_decode($description))) .'" />' . "\n";
       }
 
-      // OpenGraph
-      echo '<meta property="og:title" content="'.strip_tags(html_entity_decode($title)).'" />'."\n";
-      echo '<meta property="og:description" content="'.strip_tags(html_entity_decode($description)).'" />'."\n";
-
-      if (strlen($image_url) > 0){
-        echo '<meta property="og:image" content="'.$image_url.'" />'."\n";
+      if ($imageUrl) {
+        echo '<meta property="twitter:image" content="' . $imageUrl . '" />' . "\n";
+        echo '<meta property="og:image" content="' . $imageUrl . '" />' . "\n";
       }
     }
-
   }
 
   public function hookConfig($args)
