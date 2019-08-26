d93d225f470bc5ef9209cbbc805c693ddedde4af
Sergei Kalugin (Work iMac) <sergei.kalugin@northwestern.edu>
2019-08-26 15:02:38 -0500

    Reformat tags

--- OpenGraphTwitterMetatagsPlugin.php
+++ OpenGraphTwitterMetatagsPlugin.php
@@ -145,21 +145,21 @@ class OpenGraphTwitterMetatagsPlugin extends Omeka_Plugin_AbstractPlugin
     }
 
     // Finally, writing meta tags
-    echo '<meta property="twitter:card" content="summary_large_image" />' . "\n";
-    echo '<meta property="twitter:site" content="' . get_option(OG_TWITTER_METATAGS_PLUGIN_OPTION) . '" />' . "\n";
+    echo '<meta name="twitter:card" content="summary_large_image" />';
+    echo '<meta name="twitter:site" content="', get_option(OG_TWITTER_METATAGS_PLUGIN_OPTION), '" />';
 
     if ($title) {
-      echo '<meta property="twitter:title" content="' . strip_tags($title) . '" />' . "\n";
-      echo '<meta property="og:title" content="' . htmlentities(strip_tags(html_entity_decode($title))) . '" />' . "\n";
+      echo '<meta name="twitter:title" content="', strip_tags($title), '" />';
+      echo '<meta name="og:title" content="', htmlentities(strip_tags(html_entity_decode($title))), '" />';
 
       if ($description) {
-        echo '<meta property="twitter:description" content="' . strip_tags($description) . '" />' . "\n";
-        echo '<meta property="og:description" content="' . htmlentities(strip_tags(html_entity_decode($description))) .'" />' . "\n";
+        echo '<meta name="twitter:description" content="', strip_tags($description), '" />';
+        echo '<meta name="og:description" content="', htmlentities(strip_tags(html_entity_decode($description))), '" />';
       }
 
       if ($imageUrl) {
-        echo '<meta property="twitter:image" content="' . $imageUrl . '" />' . "\n";
-        echo '<meta property="og:image" content="' . $imageUrl . '" />' . "\n";
+        echo '<meta name="twitter:image" content="', $imageUrl, '" />';
+        echo '<meta name="og:image" content="', $imageUrl, '" />';
       }
     }
   }
